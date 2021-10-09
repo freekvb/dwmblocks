615d250a2f1ac16d55746c38ce1d7e2d53220b34
freekvb <freekvb@gmail.com>
2021-10-09 02:57:57 +0200

    date mem

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,26 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/	    /*Command*/		/*Update Interval*/	    /*Update Signal*/
+
+    {"  [cpu ",     "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}' ",    1,    0 },
+
+    {"temp ",       "sensors | awk '/^Package/ { print $4+0\"°C\" }' ",    1,    0 },
+
+    {"load ",       "cat /proc/loadavg | awk '{print $1, $2, $3}' ",    1,    0 },
+
+    {"mem ",        "free -h | awk '/^Mem/ { print $3\" / \"$2 }' | sed s/i//g ",    5,	   0 },
+
+    {"upd ",        "checkupdates | wc -l ",    10,    0 },
+
+    {"mail ",       "mail_check ",    10,    0 },
+
+    {"vol ",        "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1 ",    1,    0 },
+
+	{"",            "date '+%a %d %b %H:%M']",    10,    0 },
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "] [";
 static unsigned int delimLen = 5;
