9f80d1a52c3ed40c3dc6ab6d1c5cb41dffdee72d
freekvb <freekvb@gmail.com>
2021-12-29 21:33:45 +0100

    esthetics

--- blocks.def.h
+++ blocks.def.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
     /*Icon*/	    /*Command*/		/*Update Interval*/	    /*Update Signal*/
 
 
-    {"  [cpu ",     "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}' ",    1,    0 },
+    {"   cpu ",     "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}' ",    1,    0 },
 
     {"temp ",       "sensors | awk '/^Package/ { print $4+0\"°\" }' ",    1,    0 },
 
@@ -18,10 +18,10 @@ static const Block blocks[] = {
 
     {"vol ",        "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1 ",    1,    0 },
 
-	{"",            "date '+%a %d %b %H:%M']",    10,    0 },
+	{"",            "date '+%a %d %b %H:%M ' ",    10,    0 },
 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "]  [";
+static char delim[] = "    ";
 static unsigned int delimLen = 5;
