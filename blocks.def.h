c231fdddf58bbf12271a390efead00140e726d65
freekvb <freekvb@gmail.com>
2023-04-21 00:52:04 +0200

    looks

--- blocks.def.h
+++ blocks.def.h
@@ -4,13 +4,13 @@ static const Block blocks[] = {
     /*Icon*/	    /*Command*/		/*Update Interval*/	    /*Update Signal*/
 
 
-    {"  cpu ",     "top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)\"%\" }' ",    1,    0 },
+    {"   cpu ",     "top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)\"%\" }' ",    1,    0 },
 
     {"temp ",       "sensors | awk '/^Package/ { print $4+0\"°C\" }' ",    1,    0 },
 
     {"load ",       "cat /proc/loadavg | awk '{ print $1, $2, $3 }' ",    1,    0 },
 
-    {"mem ",        "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g ",    5,	   0 },
+    {"ram ",        "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g ",    5,	   0 },
 
     {"root ",       "df -hl / | awk '{ print $5 }' | tail -n 1 ",   60,    0 },
 
@@ -22,7 +22,7 @@ static const Block blocks[] = {
 
     {"vol ",        "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1 ",    1,    0 },
 
-    {"",            "date '+%a %d %b %y  %H:%M ' ",    10,    0 },
+    {"",            "date '+%a %d %b %Y  %H:%M ' ",    10,    0 },
 
 };
 
