f73da43868c43836eb665f19a8fb10cdf7c11e08
freekvb <freekvb@gmail.com>
2023-05-19 04:02:54 +0200

    simplified

--- blocks.def.h
+++ blocks.def.h
@@ -4,28 +4,28 @@ static const Block blocks[] = {
     /*Icon*/	    /*Command*/		/*Update Interval*/	    /*Update Signal*/
 
 
-    {"   cpu ",     "top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)\"%\" }' ",    1,    0 },
+    /*{"   cpu ",     "top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)\"%\" }' ",    1,    0 },
 
-    {"temp ",       "sensors | awk '/^Package/ { print $4+0\"°C\" }' ",    1,    0 },
+     *{"temp ",       "sensors | awk '/^Package/ { print $4+0\"°C\" }' ",    1,    0 },
 
-    {"load ",       "cat /proc/loadavg | awk '{ print $1, $2, $3 }' ",    1,    0 },
+     *{"load ",       "cat /proc/loadavg | awk '{ print $1, $2, $3 }' ",    1,    0 },
 
-    {"ram ",        "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g ",    5,	   0 },
+     *{"ram ",        "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g ",    5,	   0 },
 
-    {"root ",       "df -hl / | awk '{ print $5 }' | tail -n 1 ",   60,    0 },
+     *{"root ",       "df -hl / | awk '{ print $5 }' | tail -n 1 ",   60,    0 },
 
-    {"home ",       "df -hl /home | awk '{ print $5 }' | tail -n 1 ",   60,    0 },
+     *{"home ",       "df -hl /home | awk '{ print $5 }' | tail -n 1 ",   60,    0 },
+     */
+    {"upd ",        "checkupdates | wc -l",    10,    0 },
 
-    {"upd ",        "checkupdates | wc -l ",    10,    0 },
+    {"mail ",       "mail_check",    10,    0 },
 
-    {"mail ",       "mail_check ",    10,    0 },
+    {"vol ",        "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1",    1,    0 },
 
-    {"vol ",        "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1 ",    1,    0 },
-
-    {" ",            "date '+%a %d %b %Y  %H:%M ' ",    10,    0 },
+    {"",           "date '+%a %d %H:%M ' ",    10,    0 },
 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "   ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
